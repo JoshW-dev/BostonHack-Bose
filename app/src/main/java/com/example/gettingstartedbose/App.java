aed7a0f45b1c8e5e0e4f00ae15febd4e5962d536
Josh Wade <joshuafwade@gmail.com>
2019-11-16 17:20:10 -0500

    add start console message

--- app/src/main/java/com/example/gettingstartedbose/App.java
+++ app/src/main/java/com/example/gettingstartedbose/App.java
@@ -9,6 +9,7 @@ public class App extends Application {
 
     @Override
     public void onCreate() {
+        System.out.println("Started");
         super.onCreate();
         BoseWearable.configure(this, new Config.Builder().build());
     }

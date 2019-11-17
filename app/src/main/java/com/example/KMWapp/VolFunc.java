5d950ffec303c6eadbc4c08cf9827bf5dc8b15b8
Daniel Molyneaux <danie@DanielsSurfaceBook.BeaverCal.local>
2019-11-16 19:27:38 -0500

    commented my error cause im dumb;

--- app/src/main/java/com/example/KMWapp/VolFunc.java
+++ app/src/main/java/com/example/KMWapp/VolFunc.java
@@ -5,7 +5,7 @@ import android.media.AudioManager;
 
 public class VolFunc {
     public static void changeVol() {
-        AudioManager audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+//        AudioManager audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
 
         System.out.println("This function changes volume based on head rotation");
 //        audio = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
